97b2f8103185d90a775abe6660cb4c073d359fb2
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-05-30 12:12:24 +0200

    FEAT: add deploy

---
+++ deploy.py
@@ -0,0 +1,62 @@
+import subprocess
+import requests
+
+username = 'Florent'
+token = '9887f29efdf8a0378c44d3982a851be94dd1cf2c'
+id= '34020008'
+domain_name= 'florent.pythonanywhere.com'
+
+# response = requests.get(
+#     'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+#         username=username
+#     ),
+#     headers={'Authorization': 'Token {token}'.format(token=token)}
+# )
+# if response.status_code == 200:
+#     print('CPU quota info:')
+#     print(response.content)
+# else:
+#     print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+
+def pull():
+    response = requests.post(
+        'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/{id}/send_input/'.format(
+            username=username,
+            id=id
+        ),
+        headers={'Authorization': 'Token {token}'.format(token=token)}
+    )
+    if response.status_code == 200:
+        print('CPU quota info:')
+        print(response.content)
+    else:
+        print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+
+def reload():
+    response = requests.post(
+        'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain_name}/reload/'.format(
+            username=username,
+            domain_name=domain_name
+        ),
+        headers={'Authorization': 'Token {token}'.format(token=token)}
+    )
+    if response.status_code == 200:
+        print('CPU quota info:')
+        print(response.content)
+    else:
+        print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+
+
+def push():
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", "CI/CD"])
+    subprocess.run(["git", "push", "origin", "main"])
+
+result = subprocess.run(["pytest"], shell=True, capture_output=True, text=True)
+
+if result.returncode:
+    print("ERROR")
+else:
+    print("OK")
+    pull()
+    reload()
--- tests/test_function.py
+++ tests/test_function.py
@@ -1,1 +1,7 @@
 # TEST
+
+def calc(i):
+    return i + 4
+
+def test_calc():
+    assert calc(1) == 5
