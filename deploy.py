50e58b9c29974d520b4f3126a97b7eb0cbaa176c
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-05-30 13:28:34 +0200

    FEAT: CI/CD

--- deploy.py
+++ deploy.py
@@ -3,14 +3,14 @@ import requests
 
 username = 'Florent'
 token = '9887f29efdf8a0378c44d3982a851be94dd1cf2c'
-id= '34020008'
-domain_name= 'florent.pythonanywhere.com'
+id = '34020008'
+domain_name = 'florent.pythonanywhere.com'
 
 response = requests.get(
     'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
-        username=username
+        username = username
     ),
-    headers={'Authorization': 'Token {token}'.format(token=token)}
+    headers = {'Authorization': 'Token {token}'.format(token=token)}
 )
 if response.status_code == 200:
     print('CPU quota info:')
@@ -18,7 +18,7 @@ if response.status_code == 200:
 else:
     print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
 
-result = subprocess.run(["pytest"], shell=True, capture_output=True, text=True)
+result = subprocess.run(["pytest"], shell = True, capture_output = True, text = True)
 
 def push():
     subprocess.run(["git", "add", "."])
@@ -28,30 +28,22 @@ def push():
 def pull():
     response = requests.post(
         'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/{id}/send_input/'.format(
-            username=username,
-            id=id
+            username = username,
+            id = id
         ),
-        headers={'Authorization': 'Token {token}'.format(token=token)}
+        headers = {'Authorization': 'Token {token}'.format(token = token),
+                   'Content-Type': 'application/json'},
+        json = {'input': 'cd ~/mysite && git pull\n'}
     )
-    if response.status_code == 200:
-        print('CPU quota info:')
-        print(response.content)
-    else:
-        print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
 
 def reload():
     response = requests.post(
         'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain_name}/reload/'.format(
-            username=username,
-            domain_name=domain_name
+            username = username,
+            domain_name = domain_name
         ),
-        headers={'Authorization': 'Token {token}'.format(token=token)}
+        headers = {'Authorization': 'Token {token}'.format(token = token)}
     )
-    if response.status_code == 200:
-        print('CPU quota info:')
-        print(response.content)
-    else:
-        print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
 
 if result.returncode:
     print("ERROR")
